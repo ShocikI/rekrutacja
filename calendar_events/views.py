a67b7bdc3a76a958987002a5c545bda0a4faf5df
ShocikI <szotmicha16@gmail.com>
2024-09-30 16:54:38 +0200

    Dodanie komentarzy

--- calendar_events/views.py
+++ calendar_events/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from datetime import date, timedelta, datetime
 from calendar import monthrange
+
 from rekrutacja import settings
 
 
-# Create your views here.
 def start(request):
     today = date.today()
     year = today.year
@@ -15,20 +15,18 @@ def start(request):
 
     return redirect(reverse(calendar_view, kwargs={'year':year, 'month':month}))
 
+
 def start_year(request, year: str):
-    try:
-        year = int(year)
-    except:
-        return redirect('')
+    year = int(year)
     return redirect(reverse(calendar_view, kwargs={'year':f"{year}", 'month':"1"}))
     
+
 def calendar_view(request, year: str, month: str):
-    try:
-        year = int(year)   
-        month = int(month) 
-    except:
-        return redirect('')
-    
+    # Przekonwertowanie str na int
+    year = int(year)   
+    month = int(month) 
+
+    # Ograniczenie wartości miesiąca na 1-12
     if month < 1:
         month = 1
     if month > 12:
@@ -52,7 +50,7 @@ def calendar_view(request, year: str, month: str):
         'empty_days': empty_days,
     }
 
-    # Przygotowanie słownika, gdzie kluczem będzie dzień, a wartością lista wydarzeń
+    # Przygotowanie dictionary, gdzie kluczem będzie dzień, a wartością lista wydarzeń
     events_by_day = {day: [] for day in range(1, num_days_in_month + 1)}
 
     # Jeśli nie udało się połączyć z API wyświetlany jest 'pusty' kalendarz + error
@@ -71,8 +69,6 @@ def calendar_view(request, year: str, month: str):
 
     # Przygotowanie listy wydarzeń na bieżący miesiąc
     events_in_month = []
-
-    # Przetwarzanie wydarzeń
     for event in all_events:
         event_start_date = datetime.strptime(event['start_time'], '%Y-%m-%dT%H:%M:%S')
         if event_start_date.month == month and event_start_date.year == year:
@@ -92,11 +88,12 @@ def calendar_view(request, year: str, month: str):
             if event['start_date'].day == i:
                 events_by_day[i].append(event)
 
+    # Wyświetlenie danych w kalendarzu
     return render(request, 'calendar_view.html', {
-        'year':year, 
-        'month':month, 
+        'year': year, 
+        'month': month, 
         'calendar': calendar_data,
-        'events':events_by_day, 
+        'events': events_by_day, 
         'previous_year': previous_year,
         'next_year': next_year,
         'previous_month': previous_month,
@@ -105,24 +102,26 @@ def calendar_view(request, year: str, month: str):
     
 
 def day_view(request, year: str, month: str, day: str):
-    try:
-        year = int(year)   
-        month = int(month) 
-        day = int(day)
-    except:
-        return redirect('')
+    # Przekonwertowanie str na int
+    year = int(year)
+    month = int(month)
+    day = int(day)
+
+    # Ograniczenie wartości miesiąca na 1-12
     if month < 1:
         month = 1
     if month > 12:
         month = 12
 
+    # Ograniczenie wartości dnia do możliwych
     _, num_days_in_month = monthrange(year, month)
     if day > num_days_in_month:
         day = num_days_in_month
+
     # Połączenie z API
     all_events = get_events()
 
-    # Przygotowanie danych do poprawnych zmian miesięcy w kalendarzu
+    # Przygotowanie danych do poprawnego wyświetlenia nazwy miesiąca
     current_month_name, *_ = get_date_data(year, month)
     calendar_data = {
         'month_name': current_month_name,
@@ -149,11 +148,16 @@ def day_view(request, year: str, month: str, day: str):
             and event_start_date.month == month 
             and event_start_date.day <= day <= finish_day.day
         ):
+            # Jeśli dany event ma miejsce danego dnia zostają ściągnięte jego dane 
             current_event = get_event_by_id(event_id=event['id'])
             if current_event != None:
-                event_minute = f"0{event_start_date.minute}" if event_start_date.minute < 10 else f"{event_start_date.minute}"
-                start_time = f"{event_start_date.hour}:{event_minute}"
+                if event_start_date.day == day:
+                    event_minute = f"0{event_start_date.minute}" if event_start_date.minute < 10 else f"{event_start_date.minute}"
+                    start_time = f"{event_start_date.hour}:{event_minute}"
+                else: 
+                    start_time = f""
                 tags = [tag for tag in current_event['tags']]
+                image_url = f"{settings.BASE_URL}/{current_event['image_url']}"
                 day_events.append({
                     'id': current_event['id'],
                     'name': current_event['name'],
@@ -162,9 +166,10 @@ def day_view(request, year: str, month: str, day: str):
                     'long_description': current_event['long_description'],
                     'link': current_event['registration_link'],
                     'tags': tags,
-                    'image_url': f"{settings.BASE_URL}/{current_event['image_url']}"
+                    'image_url': image_url
                 })
 
+    # Wyświetlenie danych w podstronie konkretnego dnia
     return render(request, 'day_view.html', {
         'year': year, 
         'month': month, 
@@ -175,10 +180,12 @@ def day_view(request, year: str, month: str, day: str):
     
 
 def not_found_view(request, exception):
+    # Wyświetlenie strony gdy nie znaleziono adresu url
     return render(request, '404.html', status=404)
 
 # Połączenia z API
 def get_events():
+    # Zebranie wszystkich danych z API
     url = f"{settings.BASE_URL}/events"
     headers = {
         'api-key': settings.API_KEY,
@@ -193,6 +200,7 @@ def get_events():
 
 
 def get_event_by_id(event_id:int):
+    # Zebranie danych pojedynczego eventu z API
     url = f"{settings.BASE_URL}/events/{event_id}"
     headers = {
         'api-key': settings.API_KEY,
@@ -207,6 +215,7 @@ def get_event_by_id(event_id:int):
 
 # Funkcje użytkowe
 def get_month_name(month: int) -> str:
+    # Zebranie nazwy danego miesiąca w języku polskim
     match month:
         case 1: return "Styczeń"
         case 2: return "Luty"
--- rekrutacja/settings.py
+++ rekrutacja/settings.py
@@ -29,7 +29,6 @@ DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
